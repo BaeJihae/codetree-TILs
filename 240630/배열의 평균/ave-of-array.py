bcad8cdf507111a9ecbfd4048717ba185f8c2d13
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-06-30 01:17:55 +0900

    Title: 배열의 평균, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240630/배열의 평균/ave-of-array.py
+++ 240630/배열의 평균/ave-of-array.py
@@ -2,12 +2,12 @@ arr_2nd = [list(map(int, input().split())) for _ in range(2)]
 
 total_sum = 0
 
-for arr in arr_2nd:
+for i in range(2):
     sum = 0
-    for i in arr:
-        sum += i
+    for j in range(4):
+        sum = sum + arr_2nd[i][j]
     total_sum += sum
-    print(sum / 4.0, end = " ")
+    print(round(sum / 4.0, 1), end = " ")
 
 print()
 
@@ -15,8 +15,8 @@ for i in range(4):
     sum = 0
     for j in range(2):
         sum = sum + arr_2nd[j][i]
-    print(sum / 2.0, end = " ")
+    print(round(sum / 2.0, 1), end = " ")
 
 print()
 
-print(total_sum / 8.0)
+print(round(total_sum / 8.0, 1))
