69e6d4054d850787552305c87e666db6d58c5272
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-07-01 20:59:52 +0900

    Title: 대각선으로 숫자 채우기, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240701/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -8,7 +8,7 @@ num = 1
 for i in range(n+m-1):
     row, col = 0, i
     for _ in range(i+1):
-        if row < m and col < n:
+        if row < n and col < m:
             list[row][col] = num
             num += 1
         row += 1
